f718228b07d5d71e194d45590cd5a0f5f7285e75
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-03-14 20:11:38 -0400

    Initial commit with rough attempt

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,66 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
-import * as React from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled, { createGlobalStyle } from 'styled-components';
 
-import Header from "./header"
-import "./layout.css"
+const GlobalStyle = createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  body {
+    line-height: 1;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  blockquote, q {
+    quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  html, body, #___gatsby, #gatsby-focus-wrapper {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    color: #444444;
+    font-family: monospace;
+  }
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,27 +74,10 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <Layout.Root>
+      <GlobalStyle />
+      <Layout.Main>{children}</Layout.Main>
+    </Layout.Root>
   )
 }
 
@@ -52,4 +85,15 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+Layout.Root = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+Layout.Main = styled.main`
+  flex: 1;
+  display: flex;
+`;
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,78 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const sentences = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Aenean at ante egestas, cursus augue malesuada, fringilla ipsum.',
+  'Aenean pretium odio ac tortor dictum, a maximus lectus vestibulum.',
+  'Donec quis mi at magna fermentum maximus.',
+  'Quisque ut tellus ut neque interdum porta at non eros.',
+  'Curabitur a lacus finibus, auctor velit in, elementum mauris.',
+  'Aliquam viverra ipsum ac lacus viverra pulvinar.',
+  'Phasellus lacinia enim congue pellentesque facilisis.',
+  'Donec dapibus mauris et ullamcorper convallis.',
+  'Morbi non erat hendrerit mi scelerisque auctor ut et sem.',
+  'Pellentesque quis libero id dolor dapibus dapibus.',
+  'Aliquam in arcu porttitor, pretium nisi vitae, euismod orci.',
+  'Fusce quis lectus a lectus aliquet maximus.',
+  'Nulla a purus eget magna blandit rhoncus quis et nisi.',
+  'Praesent varius ipsum sit amet lorem pulvinar, eget tempor nibh dictum.',
+  'Nunc dapibus sem et cursus malesuada.',
+  'Vestibulum vitae nibh posuere arcu elementum maximus in at nulla.',
+  'Nunc dapibus eros quis aliquet pellentesque.',
+  'Donec efficitur odio vel ante eleifend, dapibus pellentesque felis blandit.',
+  'Curabitur porttitor mauris nec libero suscipit, et pulvinar purus mattis.',
+  'Nunc pretium dui eget nulla congue, et fermentum nulla porttitor.',
+  'Nam aliquet enim eget cursus ultrices.',
+];
 
-export default IndexPage
+const IndexPage = () => {
+  const createTextRows = () => {
+    const allCharactersInSubmission = sentences.join(' ').split('');
+
+    const textForRows = [];
+    let characterIndex = 0;
+    let rowCount = 1;
+    let rowScalingFactor = 16;
+    while (characterIndex <= allCharactersInSubmission.length) {
+      const indexOfLastCharacterInRow = characterIndex + (16 * rowCount);
+      const rowText = allCharactersInSubmission.slice(characterIndex, indexOfLastCharacterInRow).join('');
+      
+      textForRows.push(rowText);
+      rowCount++;
+      characterIndex = indexOfLastCharacterInRow;
+    }
+
+    console.log(textForRows);
+
+    return textForRows.map((rowText, idx) => <IndexPage.TextRow rowIndex={idx + 1}>{rowText}</IndexPage.TextRow>);
+  };
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <IndexPage.Content>
+        {createTextRows()}
+      </IndexPage.Content>
+    </Layout>
+  );
+};
+
+IndexPage.Content = styled.div`
+  flex: 1;
+  display: grid;
+  grid-template-columns: 1fr;
+`;
+
+IndexPage.TextRow = styled.p`
+  text-transform: uppercase;
+  line-height: 0.7;
+  font-size: calc(10vw * ${(p) => 1 / p.rowIndex});
+`;
+
+
+export default IndexPage;
